0400c1ea51bc2bebfcf994049132b54ea59af46d
Jörg Janssen <56492407+j-janssen@users.noreply.github.com>
2022-01-16 18:44:56 +0100

    Add files via upload

---
+++ Triangle.py
@@ -0,0 +1,127 @@
+import Triangulation
+import math
+import random
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image       #for image input
+import os                   #for image input - image path
+import drawSvg as draw      #for image output
+
+class SOM(object):
+
+    def __init__(self, pic_path, fineness):
+        im = Image.open(pic_path)
+        self.pic_width, self.pic_height = im.size
+        self.pic_width = int(self.pic_width) -1
+        self.pic_height = int(self.pic_height) -1
+        self.rgb_pic = im.convert('RGB')
+        X = np.round(np.linspace(0,self.pic_width -1, fineness))
+        Y = np.round(np.linspace(0,self.pic_height -1 , fineness))
+        self.weights = np.array([[
+                                [x,y,self.rgb_pic.getpixel((x, y))[0],self.rgb_pic.getpixel((x, y))[1],self.rgb_pic.getpixel((x, y))[2]]
+                                for x in X] for y in Y])
+        t_pts_X = np.round(np.linspace(0,self.pic_width -1, 100)) 
+        t_pts_Y = np.round(np.linspace(0,self.pic_height -1, 100))
+        self.train_pts = np.array([[
+                                [int(x),int(y),self.rgb_pic.getpixel((x, y))[0],self.rgb_pic.getpixel((x, y))[1],self.rgb_pic.getpixel((x, y))[2]]
+                                for x in t_pts_X] for y in t_pts_Y])
+        self.fineness = fineness
+
+    def get_dist(self, A, B):
+        dist = abs(A[0] - B[0]) + abs(A[1]-B[1]) 
+        temp = min(abs(B[0]-A[0]), abs(B[1]-A[1]))
+        dist = dist - 0.5 * abs(np.sign(B[0]-A[0]) * temp - np.sign(B[1]-A[1])* temp)
+        return dist
+
+    def get_winner(self, point):
+        k,l = (0,0)
+        min = np.linalg.norm(np.subtract(point, self.weights[0][0]))
+        for i in range(self.fineness):
+            for j in range(self.fineness):
+                temp = np.linalg.norm(np.subtract(point, self.weights[i][j]))
+                if(temp < min):
+                    min = temp
+                    k,l = (i,j)
+        return (k,l)
+
+    def dist_fct(self, dist, rad):
+        if(dist > rad): 
+            return 0
+        else:
+            return 1- dist/rad
+
+    def training(self, max_iteration, learn_rate):
+        rad = 3
+        max_iteration = max_iteration
+        for stepp in tqdm(range(max_iteration)):
+            x = random.randint(0,99)
+            y = random.randint(0,99)
+            self.train_pts[y][x]
+            k,l = self.get_winner(self.train_pts[y][x])
+            for i in range(1,self.fineness-1):
+                for j in range(1,self.fineness-1):
+                    if(self.dist_fct(self.get_dist([i,j],[k,l]), rad) != 0):
+                        self.weights[i][j] += learn_rate * np.subtract( self.train_pts[y][x] ,self.weights[i][j]) * self.dist_fct(abs(i-k)+abs(j-l), rad)
+                rad = rad * 0.9999
+
+    def gen_image(self,abs_file_path):
+        list1 = [self.train_pts[0][0]]
+        list2 = [self.train_pts[0][99]]
+        list3 = [self.train_pts[99][99]]
+        list4 = [self.train_pts[99][0]]
+        for i in range(1,98):
+            if(self.get_winner(self.train_pts[0][i]) != self.get_winner(self.train_pts[0][i+1])):
+                list1 += [self.train_pts[0][i]]
+            if(self.get_winner(self.train_pts[i][99]) != self.get_winner(self.train_pts[i+1][99])):
+                list2 += [self.train_pts[i][99]]
+            if(self.get_winner(self.train_pts[99][99-i]) != self.get_winner(self.train_pts[99][99-(i+1)])):
+                list3 += [self.train_pts[99][99-i]]
+            if(self.get_winner(self.train_pts[99-i][0]) != self.get_winner(self.train_pts[99-(i+1)][0])):
+                list4 += [self.train_pts[99-i][0]]
+        list1 = list1 + list2 + list3 + list4
+        D = Triangulation.Triangle(list1[0][0],list1[0][1],list1[1][0],list1[1][1],self.train_pts[49][49][0],self.train_pts[49][49][1])
+        D.initialize(list1, self.train_pts[49][49][0],self.train_pts[49][49][1], D)
+        
+        for i in range(1,99):
+            for j in range(1,99):
+                if(self.get_winner(self.train_pts[i][j]) != self.get_winner(self.train_pts[i][j+1])):
+                    D.add_point(self.train_pts[i][j][0],self.train_pts[i][j][1],D)
+                    D.re_delaunay_prop(D)
+                if(self.get_winner(self.train_pts[j][i]) != self.get_winner(self.train_pts[j+1][i])):
+                    D.add_point(self.train_pts[j][i][0],self.train_pts[j][i][1],D)
+                    D.re_delaunay_prop(D)
+
+        d = draw.Drawing(self.pic_width, self.pic_height, origin='center', displayInline=False)
+        w = (self.pic_width -1)/2
+        h = (self.pic_height -1)/2
+        for y in range(99):
+            for x in range(99):
+                if(D.get_triangle_via_point(self.train_pts[x][y][0]+1,self.train_pts[x][y][1]+1 ,D)):
+                    x1, x2, y1,y2, z1,z2 = D.get_triangle_via_point(self.train_pts[x][y][0]+1,self.train_pts[x][y][1]+1 ,D)
+                    k,l = self.get_winner(self.train_pts[x][y])
+                    d.append(draw.Lines(int(x1-w),int(h-x2),
+                                        int(y1-w),int(h-y2),
+                                        int(z1-w),int(h-z2),
+                                        int(x1-w),int(h-x2),
+                                        close=False,
+                                        fill='#%02x%02x%02x' % (int(self.weights[k][l][2]),int(self.weights[k][l][3]),int(self.weights[k][l][4])) ,
+                                        ))
+        d.setPixelScale(1)
+        d.saveSvg(abs_file_path[0:-4] + '_pt.svg')   #is saved in home directory
+        d.savePng(abs_file_path[0:-4] + '_pt.png')   #is saved in home directory
+
+            
+rel_path = input('Please type your relative path of your picture! Exp: Images/test_pic_01.jpg : ')
+script_dir = os.path.dirname(__file__) 
+abs_file_path = os.path.join(script_dir, rel_path)
+fineness = 5
+max_iteration = 500000
+lear_rate = 0.1
+print('Preprocessing starts!')
+
+
+NN = SOM(abs_file_path, fineness)
+NN.training(max_iteration, lear_rate)
+NN.gen_image(abs_file_path)
+
---
+++ Triangulation.cpp
@@ -0,0 +1,363 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
+#include <pybind11/stl.h>
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <list>
+#include <algorithm>
+#include <memory>
+
+using namespace std;
+
+namespace py = pybind11;
+
+class Point{
+
+    public:
+        Point()                             = delete;
+        Point(int a, int b): x(a),y(b){};
+        Point(const Point& p)               = default;
+        Point& operator=(const Point& p)    = default;
+        Point(Point&& p)                    = default;
+        Point& operator=(Point&& p)         = default;
+        ~Point()                            = default;
+
+        int& operator[](int i);
+        Point& operator-=(Point b);
+        Point& operator+=(Point b);
+        
+    private:
+        int x,y;
+
+};
+
+int& Point::operator[](int i){
+
+    if(i == 0) return x;
+    return y;
+}
+
+Point& Point::operator-=(Point b){
+    x -= b[0];
+    y -= b[1];
+    return *this;
+}
+
+Point& Point::operator+=(Point b){
+    x += b[0];
+    y += b[1];
+    return *this;
+}
+
+Point operator-(Point a, Point b){
+    return a-= b;
+}
+
+Point operator+(Point a, Point b){
+    return a+= b;
+}
+
+int operator*(Point a, Point b){
+    return a[0]*b[0] + a[1]*b[1];
+}
+
+bool operator==(Point a, Point b){
+    return (a[0] == b[0] && a[1]==b[1]);
+}
+
+bool operator!=(Point a, Point b){
+    return !(a==b);
+}
+
+int det(int mat[3][3]){
+    int d   = mat[0][0] * (mat[1][1] * mat[2][2] - mat[1][2] * mat[2][1]);
+    d       -= mat[1][0] * (mat[0][1] * mat[2][2] - mat[0][2] * mat[2][1]);
+    d       += mat[2][0] * (mat[0][1] * mat[1][2] - mat[0][2] * mat[1][1]);     
+    return d; 
+}
+
+class Triangle{
+
+    public:
+        Triangle() = delete;
+        Triangle(int a1,int a2, int b1, int b2, int c1, int c2):nbh(3), x(Point(a1,a2)), y(Point(b1,b2)), z(Point(c1,c2)){};
+        Triangle(Point a, Point b, Point c): nbh(3), x(a), y(b), z(c){};    
+        ~Triangle() = default;
+        vector<shared_ptr<Triangle>> nbh; 
+
+        vector<Point> get_pts() const;   
+        void set_pts(const Point& a, const Point& b, const Point& c);
+        std::list<int> get_points(){return {x[0],x[1],y[0],y[1],z[0],z[1]};}
+        std::list<int> get_nbh_points();
+        
+        bool pt_contained(int p1, int p2);
+        int nbh_position(shared_ptr<Triangle> A);
+        Point nbh_point(int i);
+
+        list<int> get_triangle_via_point(int x, int y, shared_ptr<Triangle> this_ptr);
+        void initialize(vector<vector<int>> T, int z1, int z2, shared_ptr<Triangle> this_ptr);                     
+        void split(int x, int y, shared_ptr<Triangle> this_ptr);
+        shared_ptr<Triangle> add_point(int x, int y, shared_ptr<Triangle> this_ptr);
+        bool delaunay_prop(int i);       
+        void flip(int i, shared_ptr<Triangle> this_ptr);
+        void re_delaunay_prop( shared_ptr<Triangle> this_ptr);                                
+
+    private:
+        Point x,y,z;
+          
+
+};
+
+
+std::list<int> Triangle::get_nbh_points(){
+    list<int> list({x[0],x[1],y[0],y[1],z[0],z[1]});
+    for(int i =0 ; i<3; i++){
+        if(nbh[i]){
+            list.push_back(nbh[i]->x[0]);
+            list.push_back(nbh[i]->x[1]);
+            list.push_back(nbh[i]->y[0]);
+            list.push_back(nbh[i]->y[1]);
+            list.push_back(nbh[i]->z[0]);
+            list.push_back(nbh[i]->z[1]);
+        }
+    }
+    return list;
+}
+
+//Get-function that returns the vertices of the triangle
+vector<Point> Triangle::get_pts() const{
+    return vector<Point>({x,y,z});
+}
+
+//Set-function for the vertices (orientation - counterclockwise)
+void Triangle::set_pts(const Point& a, const Point& b, const Point& c){
+    //assert - a,b,c are oriented counterclockwise
+    x = a;
+    y = b;
+    z = c;
+}
+
+//Returns the position of a given neighbor
+int Triangle::nbh_position(shared_ptr<Triangle> A){
+    for(int i = 0; i<3;i++){
+        if(nbh[i] == A)
+            return i;
+    }
+    return -1;  //case: not a neighbor 
+}
+
+//Returns the remaining point for a given neighbor
+Point Triangle::nbh_point(int i){
+    if(nbh[i]){
+        auto pts = nbh[i]->get_pts();
+        for(int j=0; j < 3; j ++){
+            if(pts[j] != x && pts[j] != y && pts[j] != z)
+                return pts[j];
+        }
+    }
+    return Point(0,0);  //case: there is no ith neighbor   
+}
+
+//Checks if a point is inside the given triangle
+bool Triangle::pt_contained(int p1, int p2){
+    Point p(p1,p2);
+    Point nxy((y - x)[1], -(y - x)[0]);
+    Point nyz((z - y)[1], -(z - y)[0]);
+    Point nzx((x - z)[1], -(x - z)[0]);
+    return nxy*(p-x) < 0 &&  nyz*(p-y) < 0 && nzx*(p-z) < 0;
+}
+
+// initalizes triangles attached counter clockwise wrt the centerpoint z1,z2
+void Triangle::initialize(vector<vector<int>> T, int z1, int z2, shared_ptr<Triangle> this_ptr){
+    shared_ptr<Triangle> temp = this_ptr;
+    for(int i = 1; i< int(T.size()); i++){
+        if(i == int(T.size())-1){
+            auto E = make_shared<Triangle>(Triangle(T[i][0], T[i][1], T[0][0], T[0][1], z1, z2));
+            temp->nbh[1] = E;
+            E->nbh[2] = temp;
+            E->nbh[1] = this_ptr;
+            this_ptr-> nbh[2] = E;
+        }
+        else{
+            auto E = make_shared<Triangle>(Triangle(T[i][0], T[i][1], T[i+1][0], T[i+1][1], z1, z2));
+            temp->nbh[1] = E;
+            E->nbh[2] = temp;
+            temp = E;
+        }
+    }
+    return;
+}
+
+//Replaces the given triangle that contains a given point with three new triangles
+void Triangle::split(int x, int y, shared_ptr<Triangle> this_ptr){
+    // assert (!pt_contained(x,y))
+    auto v = get_pts();
+    auto A = this_ptr;
+    auto B = nbh[1];
+    auto C = nbh[2];
+
+    A->set_pts(v[0], v[1], Point(x,y));
+    auto D = make_shared<Triangle>(Triangle(v[1],v[2], Point(x,y)));
+    auto E = make_shared<Triangle>(Triangle(Point(x,y), v[2], v[0]));
+
+    D->nbh[0] = B;
+    D->nbh[1] = E;
+    D->nbh[2] = A;
+
+    E->nbh[0] = D;
+    E->nbh[1] = C;
+    E->nbh[2] = A;
+
+    nbh[1] = D;
+    nbh[2] = E;
+
+    if(B != nullptr)
+        B->nbh[B->nbh_position(A)] = D;
+    if(C != nullptr)
+        C->nbh[C->nbh_position(A)] = E;
+    return;
+}
+
+//Adds a new point to the triangulation
+shared_ptr<Triangle> Triangle::add_point(int x, int y, shared_ptr<Triangle> this_ptr){
+    vector<shared_ptr<Triangle>> Trgls = {this_ptr};
+    queue<shared_ptr<Triangle>> Cnddts;
+    Cnddts.push( this_ptr );
+    while(Cnddts.size() != 0){
+        if(Cnddts.front()->pt_contained(x,y)){
+            Cnddts.front()->split(x,y,Cnddts.front());
+            return Cnddts.front();
+        }
+        else{
+            for(int i = 0; i<3; i++){
+                if(find(Trgls.begin(),Trgls.end(),Cnddts.front()->nbh[i]) == Trgls.end() && Cnddts.front()->nbh[i] != nullptr){
+                    Trgls.push_back(Cnddts.front()->nbh[i]);
+                    Cnddts.push(Cnddts.front()->nbh[i]);
+                }
+            }
+            Cnddts.pop();
+        }
+    }
+    return nullptr;
+}
+
+//Adds a new point to the triangulation
+list<int> Triangle::get_triangle_via_point(int x, int y, shared_ptr<Triangle> this_ptr){
+    vector<shared_ptr<Triangle>> Trgls = {this_ptr};
+    queue<shared_ptr<Triangle>> Cnddts;
+    Cnddts.push( this_ptr );
+    while(Cnddts.size() != 0){
+        if(Cnddts.front()->pt_contained(x,y)){
+            return {Cnddts.front()->get_pts()[0][0],Cnddts.front()->get_pts()[0][1], Cnddts.front()->get_pts()[1][0],Cnddts.front()->get_pts()[1][1], Cnddts.front()->get_pts()[2][0],Cnddts.front()->get_pts()[2][1] };
+        }
+        else{
+            for(int i = 0; i<3; i++){
+                if(find(Trgls.begin(),Trgls.end(),Cnddts.front()->nbh[i]) == Trgls.end() && Cnddts.front()->nbh[i] != nullptr){
+                    Trgls.push_back(Cnddts.front()->nbh[i]);
+                    Cnddts.push(Cnddts.front()->nbh[i]);
+                }
+            }
+            Cnddts.pop();
+        }
+    }
+    return list<int>();
+}
+
+//checks if the delaunay property is satisfied wrt. the given and the ith triangle
+bool Triangle::delaunay_prop(int i){
+    if(nbh[i] == nullptr)   //trivial case 
+        return true;
+    Point D = nbh_point(i);
+    int k = (i+1) % 3;
+    int l = (i+2) % 3;
+    auto x = get_pts();
+    int mat[3][3] = {   {x[k][0] - D[0], x[k][1] - D[1], (x[k][0] - D[0])*(x[k][0] - D[0]) + (x[k][1] - D[1])*(x[k][1] - D[1])},
+                        {x[l][0] - D[0], x[l][1] - D[1], (x[l][0] - D[0])*(x[l][0] - D[0]) + (x[l][1] - D[1])*(x[l][1] - D[1])},
+                        {x[i][0] - D[0], x[i][1] - D[1], (x[i][0] - D[0])*(x[i][0] - D[0]) + (x[i][1] - D[1])*(x[i][1] - D[1])}
+                    };
+    if( det(mat) > 0)
+        return false;
+    return true;
+}
+
+//Restores the delaunay property locally
+void Triangle::flip(int i, shared_ptr<Triangle> this_ptr){
+    int m = nbh[i]->nbh_position(this_ptr);
+
+    Point a = get_pts()[(i+1) % 3];
+    Point b = get_pts()[(i+2) % 3];
+    Point c = get_pts()[i];
+    Point d = nbh_point(i);
+
+    auto T_ab = nbh[(i+1) % 3];
+    auto T_bc = nbh[(i+2) % 3];
+    auto T_cd = nbh[i]->nbh[(m+1) % 3];
+    auto T_da = nbh[i]->nbh[(m+2) % 3];
+
+    nbh[i]->set_pts(a,b,d);
+    nbh[i]->nbh[0] = T_ab;
+    nbh[i]->nbh[1] = this_ptr;
+    nbh[i]->nbh[2] = T_da;
+
+    if(T_ab != nullptr)
+        T_ab->nbh[T_ab->nbh_position(this_ptr)] = nbh[i];
+    if(T_cd != nullptr)
+        T_cd->nbh[T_cd->nbh_position(nbh[i])] = this_ptr;
+
+    this_ptr->set_pts(d,b,c);
+    this_ptr->nbh[0] = nbh[i];
+    this_ptr->nbh[1] = T_bc;
+    this_ptr->nbh[2] = T_cd;
+
+    return;
+}
+
+//Restores the delaunay property globally
+void Triangle::re_delaunay_prop( shared_ptr<Triangle> this_ptr ){
+    vector<shared_ptr<Triangle>> Trgls = {this_ptr};
+    queue<shared_ptr<Triangle>> Cnddts;
+    Cnddts.push( this_ptr );
+    while(Cnddts.size() != 0){
+        for(int j = 0; j < 3; j++){
+            if(!Cnddts.front()->delaunay_prop(j)){
+                for(int i = 0; i<3; i++){
+                    if(find(Trgls.begin(),Trgls.end(),Cnddts.front()->nbh[i]) == Trgls.end() && Cnddts.front()->nbh[i] != nullptr){
+                        Trgls.push_back(Cnddts.front()->nbh[i]);
+                        Cnddts.push(Cnddts.front()->nbh[i]);
+                    }
+                }
+                for(int i = 0; i<3; i++){
+                    if(find(Trgls.begin(),Trgls.end(),Cnddts.front()->nbh[j]->nbh[i]) == Trgls.end() && Cnddts.front()->nbh[j]->nbh[i] != nullptr){
+                        Trgls.push_back(Cnddts.front()->nbh[j]->nbh[i]);
+                        Cnddts.push(Cnddts.front()->nbh[j]->nbh[i]);
+                    }
+                }
+                Cnddts.front()->flip(j, Cnddts.front());
+            }
+        }
+        Cnddts.pop();
+    }
+    return;
+}
+
+PYBIND11_MODULE(Triangulation, m){
+    py::class_<Point>(m, "Point")
+        .def(py::init<int, int>())
+        .def(py::init<const Point&>())
+        .def(py::self += py::self)
+        .def(py::self -= py::self);
+
+    py::class_<Triangle, std::shared_ptr<Triangle>>(m, "Triangle")
+        .def(py::init<int,int,int,int,int,int>())
+        .def(py::init<Point,Point,Point>())
+        .def_readwrite("nbh", &Triangle::nbh)
+        .def("add_point", &Triangle::add_point)
+        .def("initialize", &Triangle::initialize)
+        .def("re_delaunay_prop", &Triangle::re_delaunay_prop)
+        .def("get_points", &Triangle::get_points)
+        .def("get_nbh", &Triangle::get_nbh_points)
+        .def("get_triangle_via_point", &Triangle::get_triangle_via_point);
+
+
+}
